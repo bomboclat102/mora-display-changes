325ee7192205768485c6ff3f8a38c0b75f119795
showbiz0 <samuele8max@gmail.com>
2026-07-16 16:16:08 +0200

    Github Pages deployment + ran prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://showbiz0.github.io",
+  base: "/mora-display",
   vite: {
     plugins: [tailwindcss()],
   },
